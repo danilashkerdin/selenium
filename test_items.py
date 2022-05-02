8ab43086fe9bdff2948723084e722362f0e78681
danil <danilashkerdin>
2022-05-02 16:59:36 +0300

    test_items refactored

--- test_items.py
+++ test_items.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 
 class TestCaseItems:
 
-    def test_items(self, browser):
+    def test_cart_button_exists(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
-        assert browser.find_element(By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket'), "No cart button!"
+        cart_button = WebDriverWait(browser, 10).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')))
+        assert cart_button, "No cart button!"
