bc323bcbaea6c07c4f43e9f8e2b65bf38bac6a92
Danila Shkerdin <51801104+danilashkerdin@users.noreply.github.com>
2022-05-02 16:39:43 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 class TestCaseItems:
 
-    def test_items(self, browser):
+    def test_add_to_cart_button_exists(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         assert browser.find_element(By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket'), "No cart button!"
